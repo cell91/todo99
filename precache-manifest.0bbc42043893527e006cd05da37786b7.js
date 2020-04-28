7ec56a93b5f0a8018da6b7afb93e5ad5ac9b0516
Revanth Kumar <gamingkid45@gmail.com>
2020-04-28 13:15:33 +0530

    INIT

---
+++ precache-manifest.0bbc42043893527e006cd05da37786b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "061ea80616c98e3d095f10a8e08323f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74171e22a675316d9946",
+    "url": "/static/css/main.5982ca51.chunk.css"
+  },
+  {
+    "revision": "9a7fd756317dde1644f8",
+    "url": "/static/js/2.ed45ae83.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/static/js/2.ed45ae83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74171e22a675316d9946",
+    "url": "/static/js/main.58e108cd.chunk.js"
+  },
+  {
+    "revision": "87f1c2edfb6e85308f7b",
+    "url": "/static/js/runtime-main.99e26fd3.js"
+  },
+  {
+    "revision": "ba506e99f90f162f4921f82009a242a4",
+    "url": "/static/media/manrope.ba506e99.ttf"
+  }
+]);
